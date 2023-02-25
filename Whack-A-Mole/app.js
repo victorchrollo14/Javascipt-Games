891575ca8484715de7476a326068b4f8683416f3
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-02-25 17:25:37 +0530

    completed wack a mole game

--- Whack-A-Mole/app.js
+++ Whack-A-Mole/app.js
@@ -1,8 +1,12 @@
 const grid = document.querySelector('.grid');
+const start = document.querySelector('.start');
 
 let hitPosition;
 let score = 0;
-let timeLeft = 30;
+let timeLeft = 10;
+const EASY = 500;
+let timer;
+let timeLeftTimer;
 
 // functions
 function createBoard(){
@@ -33,26 +37,42 @@ function randomBoxSelect(){
 }
 
 const countDown = () => {
-    timeLeft--;
+    timeLeft -= 1;
+    document.querySelector('.time-left').innerHTML = timeLeft;
+    if(timeLeft === -1){
+        clearInterval(timer);
+        clearInterval(timeLeftTimer);
+        alert(`Game Over \n Final Score = ${score}`);
+        setTimeout(reset, 10);
+    }
+}
+
+const reset = () => {
+    window.location.reload()
 }
 
 
 
 // Execution starts here
 createBoard();
-const timer = setInterval(randomBoxSelect, 1000);
-const timeLeftTimer = setInterval(countDown, 1000);
+
+start.addEventListener("click", () => {
+    timer = setInterval(randomBoxSelect, EASY);
+    timeLeftTimer = setInterval(countDown, 1000);
+})
+
 
 let squares = document.querySelectorAll('.square');
 
 squares.forEach(square => {
-    square.addEventListener("click", () => {
+    square.addEventListener("mousedown", () => {
         if (square.id === hitPosition){
             score++;
+            document.querySelector('.result').innerHTML = score;
             console.log("you clicked on mole");
+            hitPosition = null;
         }
     })
-    
     
 });
 
